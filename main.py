39d7b41eef49663a831d2404891c524288ee5207
TYUFUN <219561606+TYUFUN@users.noreply.github.com>
2026-09-22 17:47:28 +0200

    add default values and change gitignore

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ if not os.path.exists(".env"):
     usr_token = str(input("Enter gemini token: "))
     with open (".env", "w", encoding="utf-8") as f:
         f.write(f"TOKEN=\"{usr_token}\"")
-webbrowser.open(prompt(), 2, True) 
+webbrowser.open(prompt("") or "https://www.google.com", 2, True) 
 # url, how to open 0 - в in current tab, 1 in new window, 2 in new tab
